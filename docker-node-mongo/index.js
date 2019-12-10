96712996b3c7e49bbfea27b9e9a194afacd6ab47
nikhomgithub <48146261+nikhomgithub@users.noreply.github.com>
2019-12-10 10:54:39 +0700

    Update index.js

--- docker-node-mongo/index.js
+++ docker-node-mongo/index.js
@@ -2,13 +2,10 @@ const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 const app = express();
 const server = http.createServer(app);
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // Connect to MongoDB
 mongoose
   .connect(
@@ -19,22 +16,17 @@ mongoose
   .catch(err => console.log(err));
 
 const Item = require('./models/Item');
-
 app.get('/', (req, res) => {
   Item.find()
     .then(items => res.render('index', { items }))
     .catch(err => res.status(404).json({ msg: 'No items found' }));
 });
-
 app.post('/item/add', (req, res) => {
   const newItem = new Item({
     name: req.body.name
   });
-
   newItem.save().then(item => res.redirect('/'));
 });
-
 const port = 3000;
-
 //app.listen(port, () => console.log('Server running...'));
 server.listen(port,'127.0.0.1', () => console.log('Server running...'));
